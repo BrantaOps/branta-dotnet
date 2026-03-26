d6c5d3f9bb21fe6a02c3b7fd3811633d8125697c
Kyle McCullen <kmccullen@protonmail.com>
2026-03-26 09:02:50 -0400

    fix tests

--- Branta/Enums/DestinationType.cs
+++ Branta/Enums/DestinationType.cs
@@ -1,8 +1,32 @@
+using System.Reflection;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Branta.Enums;
 
-[JsonConverter(typeof(JsonStringEnumConverter))]
+public class DestinationTypeConverter : JsonConverter<DestinationType>
+{
+    public override DestinationType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        foreach (var field in typeof(DestinationType).GetFields(BindingFlags.Public | BindingFlags.Static))
+        {
+            var attr = field.GetCustomAttribute<JsonPropertyNameAttribute>();
+            if ((attr?.Name ?? field.Name) == value)
+                return (DestinationType)field.GetValue(null)!;
+        }
+        throw new JsonException($"Unknown DestinationType: {value}");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DestinationType value, JsonSerializerOptions options)
+    {
+        var field = typeof(DestinationType).GetField(value.ToString())!;
+        var attr = field.GetCustomAttribute<JsonPropertyNameAttribute>();
+        writer.WriteStringValue(attr?.Name ?? value.ToString());
+    }
+}
+
+[JsonConverter(typeof(DestinationTypeConverter))]
 public enum DestinationType
 {
     [JsonPropertyName("bitcoin_address")]
